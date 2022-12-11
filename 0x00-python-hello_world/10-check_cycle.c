51c1b765853f47db93fe0fbe6729f86839a4cebc
chibunduuchechi <chibunduuchechiahamefula@gmail.com>
2022-12-11 09:47:29 -0800

    checks if a singly linked list has a cycle in it

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -20,7 +20,6 @@ size_t print_listint(const listint_t *h)
         current = current->next;
         n++;
     }
-
     return (n);
 }
 
@@ -37,7 +36,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
     new = malloc(sizeof(listint_t));
     if (new == NULL)
         return (NULL);
-
+	
     new->n = n;
     new->next = *head;
     *head = new;
