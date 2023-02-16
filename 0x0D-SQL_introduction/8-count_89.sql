5f573b4b6f0224eadf2c663ac882a12202398c66
chibunduuchechi <chibunduuchechiahamefula@gmail.com>
2023-02-16 11:34:38 -0800

    counts number of records with id equals 89 in first_table

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- counts number of records with id = 89 in first_table from hbtn_0c_0 on mysql server
+-- and returns number of lines for the record
+SELECT COUNT(*) FROM first_table WHERE id = 89;
