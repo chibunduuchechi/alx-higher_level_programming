e28217dfad20242a0833074c7da9528e8c73d4fc
chibunduuchechi <chibunduuchechiahamefula@gmail.com>
2023-02-16 20:22:34 -0800

    lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
+-- lists all granted privileged for first user in a database
+SHOW GRANTS FOR user_0d_1@localhost;
+-- lists all granted privileged for second user in a database
+SHOW GRANTS FOR user_0d_2@localhost;
